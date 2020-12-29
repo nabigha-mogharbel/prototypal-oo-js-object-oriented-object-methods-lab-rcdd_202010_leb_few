c35a75acd927788c95c5ba8d245af34d29f7e2d4
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-12-29 01:03:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function BoardMember (name, homeState, training){
-  veto(){}
+veto(){ return 'No, I must disagree'}
+
+ approve(){return 'You can do that!'}
+
+ doCharity(){return  'I like to help people.'}
+
+ releasePressStatement(){return 'You will see great things from Scuber.'}
+
+ sayHi(){return "Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
 }
